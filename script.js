e074e36fb3826a968b6289272fee09e954a027b1
Alex <alex2323official@gmail.com>
2024-08-01 21:32:54 +0200

    EVERYTHING WORKS - commit before cleanup

--- script.js
+++ script.js
@@ -47,8 +47,7 @@ calculator.addEventListener("click", (e) => {
     )
     {
         // Create first number until user setup operator
-        // Tell JS that firstNumber is setup and co cleanup
-        console.log("time for operator");
+        // Tell JS that firstNumber is setup and do cleanup
         tempArray = [];
         isNumberFirstSetup = true;
 
@@ -59,32 +58,22 @@ calculator.addEventListener("click", (e) => {
         isOperatorSetup = true;
 
         // Setup 2 number if 1number and operator is setup
-    } else if(isNumberFirstSetup && isOperatorSetup)
+    } else if(isNumberFirstSetup == true && isOperatorSetup == true && isNumberSecondSetup == false && e.target.innerText != "=")
     {
+        // Second number
         tempArray.push(e.target.innerText);
         numberSecond = tempArray.reduce((accumulator, currentValue) => {
             return accumulator += currentValue;
         });
         refreshDisplay()
-    } else if(e.target.innerText == "=") {
-        
-    }
-        
-  
-    // Clear
-    if(e.target.innerText == "CLEAR") {
-        numberFirst = 0;
-        numberSecond = 0;
-        operator = "";
-        tempArray = [];
-        refreshDisplay();
-        isNumberFirstSetup = false;
-        isNumberSecondSetup = false;
-        isOperatorSetup = false;
     }
 
-    // RESULT
-    if(e.target.innerText == "=" && isNumberFirstSetup == true && isNumberSecondSetup == true && isOperatorSetup == true) {
+    // Result!
+    if(e.target.innerText == "=") {
+        // Tell JS that secondNumber is setup and do cleanup
+        tempArray = [];
+        isNumberSecondSetup = true;
+
         // Parse numbers
         numberFirst = Number(numberFirst);
         numberSecond = Number(numberSecond);
@@ -110,6 +99,21 @@ calculator.addEventListener("click", (e) => {
             result.innerText = display;
         }
     }
+        
+  
+    // Clear
+    if(e.target.innerText == "CLEAR") {
+        numberFirst = 0;
+        numberSecond = 0;
+        operator = "";
+        tempArray = [];
+        refreshDisplay();
+        isNumberFirstSetup = false;
+        isNumberSecondSetup = false;
+        isOperatorSetup = false;
+    }
+
+   
     
 })
 
